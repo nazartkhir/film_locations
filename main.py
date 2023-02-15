d2bf26fbe2af607304482d81d57d1e9cfe4dae37
Nazar Tkhir <nazar.tchir@gmail.com>
2023-02-15 19:24:54 +0200

    added description

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+"""
+This module will generate a map of closest film sets to any location.
+User has to provide the year, the coordinates and a pass to the dataset.
+"""
 import argparse
-
 def main():
     pass
