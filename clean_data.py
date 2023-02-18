6501f58eac40f1b08c127b7f28a20baf7d431d9f
Nazar Tkhir <nazar.tchir@gmail.com>
2023-02-18 16:45:36 +0200

    first 200 cords

--- clean_data.py
+++ clean_data.py
@@ -3,28 +3,43 @@ def read():
         data = file.read().splitlines()
     return data
 
-def clean(data):
-    with open('new_locations.txt', 'w',encoding='UTF-8') as file:
-        locations = set()
-        for row in data:
-            if '{' not in row:
-                if ',' in row:
-                    if ord(row[0]) in range(65, 91):
-                        if row.count('(') == 1:
-                            year = row.find('(')
-                            loc = row[year + 6:].strip()
-                            if loc in locations:
-                                continue
-                            else:
-                                locations.add(loc)
-                            if row[year+1:year+4].isnumeric():
-                                if row.find(')') - row.find('(') == 5:
-                                    if 'Federal' not in row:
-                                        tf = False
-                                        for char in row:
-                                            if ord(char) > 127:
-                                                tf = True
-                                        if not tf:
-                                            file.write(row)
-                                            file.write('\n')
-clean(read())
+def clean_and_write(data):
+    ans = []
+    locations = set()
+    names = set()
+    for row in data:
+        if '{' not in row:
+            if ',' in row:
+                if ord(row[0]) in range(65, 91):
+                    if row.count('(') == 1:
+                        year = row.find('(')
+                        loc = row[year + 6:].strip()
+                        name = row[:year].strip()
+                        if name in names:
+                            continue
+                        else:
+                            names.add(name)
+                        if loc in locations:
+                            continue
+                        else:
+                            locations.add(loc)
+                        if row[year+1:year+4].isnumeric():
+                            if row.find(')') - row.find('(') == 5:
+                                if 'Federal' not in row:
+                                    tf = False
+                                    for char in row:
+                                        if ord(char) > 127:
+                                            tf = True
+                                    if not tf:
+                                        ans.append(row)
+    with open("new_locations.csv", 'w', encoding='UTF-8') as file:
+        #file.write('name|year|address\n')
+        for row in ans:
+            first = row.find('(')
+            last = row.find(')')
+            name = row[:first].strip()
+            year = row[first + 1:last]
+            address = row[last + 1:].strip()
+            file.write(f'{name}|{year}|{address}\n')
+            
+clean_and_write(read())
--- geo.py
+++ geo.py
@@ -1,7 +1,29 @@
 from geopy.geocoders import Nominatim
 
+def read():
+    with open('new_locations.csv', encoding='UTF-8') as file:
+        data = file.read().splitlines()
+        for i, row in enumerate(data):
+            data[i] = row.split('|')
+    return data
+
 def get_cords(address):
     geolocator =Nominatim(user_agent="http")
     location = geolocator.geocode(address)
     return (location.latitude, location.longitude)
 
+def write_cords(a, b):
+    with open("new_locations_wcords.csv", 'a', encoding='UTF-8') as file:
+        data = read()[a:b]
+        for row in data:
+            try:
+                cords = get_cords(row[2])
+                row.append(str(cords[0]))
+                row.append(str(cords[1]))
+                file.write('|'.join(row))
+                file.write('\n')
+            except AttributeError:
+                continue
+            
+
+write_cords(150,200)
