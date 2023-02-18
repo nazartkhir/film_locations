cbeb602cac44182e8f1252688cb9cdb550916853
Nazar Tkhir <nazar.tchir@gmail.com>
2023-02-18 14:07:30 +0200

    final clean

--- clean_data.py
+++ clean_data.py
@@ -5,26 +5,25 @@ def read():
 
 def clean(data):
     with open('new_locations', 'w',encoding='UTF-8') as file:
-        names = set()
+        locations = set()
         for row in data:
             if '{' not in row:
-                if ord(row[0]) in range(65, 91):
-                    if row.count('(') == 1:
-                        year = row.find('(')
-                        name = row[:year].strip()
-                        if name in names:
-                            continue
-                        else:
-                            names.add(name)
-                        if row[year+1:year+4].isnumeric():
-                            if ',' in row:
+                if ',' in row:
+                    if ord(row[0]) in range(65, 91):
+                        if row.count('(') == 1:
+                            year = row.find('(')
+                            loc = row[year + 6:].strip()
+                            if loc in locations:
+                                continue
+                            else:
+                                locations.add(loc)
+                            if row[year+1:year+4].isnumeric():
                                 if 'Federal' not in row:
                                     tf = False
-                                    for chr in row:
-                                        if ord(chr) > 127:
+                                    for char in row:
+                                        if ord(char) > 127:
                                             tf = True
                                     if not tf:
                                         file.write(row)
                                         file.write('\n')
-
 clean(read())
